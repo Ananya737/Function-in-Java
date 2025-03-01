7adced2c752e700b32791b01698740fe224a9665
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-01 23:59:59 +0530

    update

--- Array.java
+++ Array.java
@@ -1,22 +1,139 @@
+
+
+
+
+
+
+
+//void
+
+
+
+
+
+// import java.util.Scanner;
+// public class Array{
+
+
+// static void array(){
+//     Scanner obj=new Scanner(System.in);
+//     System.out.println("Enter size:");
+//     int size=obj.nextInt();
+//     int arr[]=new int[size];
+
+//     for(int i=0;i<size;i++){
+//         arr[i]=obj.nextInt();
+//     }
+
+// int sum=0;
+// for(int i=0;i<arr.length;i++){
+//         sum+=arr[i];
+//     }
+
+//     System.out.print(sum);
+//     }
+
+
+
+
+
+
+
+//    public static void main(String args[]){
+//    array();
+// }
+
+// }
+
+
+
+
+
+
+
+
+//Return type:
+
+// import java.util.Scanner;
+// public class Array{
+
+// static int array(){
+ 
+//  Scanner obj=new Scanner(System.in);
+
+
+//     System.out.println("Enter size:");
+//     int size=obj.nextInt();
+//     int arr[]=new int[size];
+
+//     for(int i=0;i<size;i++){
+//         arr[i]=obj.nextInt();
+//     }
+
+// int sum=0;
+// for(int i=0;i<arr.length;i++){
+//         sum+=arr[i];
+//     }
+
+    
+//     return sum;
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public static void main(String args[]){
+// System.out.println(array());
+// }
+// }
+
+
+
+
+
+//With parameter:
+
+
 import java.util.Scanner;
 public class Array{
 
-// Sum of elements:
-
-   static void array(){
-    int [] arr=new int[]{1,2,3,4,5};
-    int sum=0;
-    for(int i=0;i<arr.length;i++){
+static boolean sums(int arr[]){
+ int sum=0;
+for(int i=0;i<arr.length;i++){
         sum+=arr[i];
     }
+
     System.out.print(sum);
+    return true;
     }
 
-    public static void main(String args[]){
-    Scanner obj=new Scanner(System.in);
-
-    array();
 
 
-   } 
+public static void main(String args[]){
+
+Scanner obj=new Scanner(System.in);
+
+System.out.println("Enter size:");
+
+int size=obj.nextInt();
+
+int arr[]=new int[size];
+
+ for(int i=0;i<size;i++){
+        arr[i]=obj.nextInt();
+    }
+
+
+sums(arr);
+}
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+int sum=0;
+// for(int i=0;i<arr.length;i++){
+//         sum+=arr[i];
+//     }
+
+//     System.out.print(sum);
+//     }
