877b4235881c8a2bd0fbcf07abe7c65bf441ea10
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-03 02:22:48 +0530

    update

--- Dup.java
+++ Dup.java
@@ -1,7 +1,134 @@
+
+
+// Non return type
+
+// import java.util.Scanner;
+// public class Dup{
+//     static void dup(){
+//         Scanner obj=new Scanner (System.in);
+//         System.out.println("Enter size :");
+//         int size=obj.nextInt();
+//         int [] arr=new int [size];
+//         for(int i=0;i<size;i++){
+//             arr[i]=obj.nextInt();
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             boolean dupli=false;
+//             for(int j=i+1;j<arr.length;j++){
+//                 if(arr[i]==arr[j]){
+//                     arr[j]=-1;
+//                     dupli=true;
+//                 }
+
+//             }
+//             if(dupli && arr[i]!=-1){
+//                 System.out.println(arr[i]);
+//             }
+//         }
+
+//     }
+//     public static void main(String args[]){
+        
+
+//         dup();
+//     }
+// }
+
+
+
+
+
+// Return type
+
+
+// import java.util.Scanner;
+// public class Dup{
+//     static int dup(){
+//         Scanner obj=new Scanner (System.in);
+//         System.out.println("Enter size :");
+//         int size=obj.nextInt();
+//         int [] arr=new int [size];
+//         for(int i=0;i<size;i++){
+//             arr[i]=obj.nextInt();
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             boolean dupli=false;
+//             for(int j=i+1;j<arr.length;j++){
+//                 if(arr[i]==arr[j]){
+//                     arr[j]=-1;
+//                     dupli=true;
+//                 }
+
+//             }
+//             if(dupli && arr[i]!=-1){
+//                 System.out.println(arr[i]);
+//             }
+//         }
+
+//         return 0;
+
+//     }
+//     public static void main(String args[]){
+        
+
+//     dup();
+//     }
+// }
+
+
+
+
+
+// Boolean 
+
+
+
+// import java.util.Scanner;
+// public class Dup{
+//     static boolean dup(){
+//         Scanner obj=new Scanner (System.in);
+//         System.out.println("Enter size :");
+//         int size=obj.nextInt();
+//         int [] arr=new int [size];
+//         for(int i=0;i<size;i++){
+//             arr[i]=obj.nextInt();
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             boolean dupli=false;
+//             for(int j=i+1;j<arr.length;j++){
+//                 if(arr[i]==arr[j]){
+//                     arr[j]=-1;
+//                     dupli=true;
+//                 }
+
+//             }
+//             if(dupli && arr[i]!=-1){
+//                 System.out.println(arr[i]);
+//             }
+//         }
+
+//         return true;
+
+//     }
+//     public static void main(String args[]){
+        
+
+//     dup();
+//     }
+// }
+
+
+
+
+
+// With parameter 
+
+
+
 import java.util.Scanner;
 public class Dup{
-    static void dup(){
-        int [] arr=new int []{1,2,3,2,3,4,5,4,5,1,6};
+    static boolean dup(int arr[]){
+       
         for(int i=0;i<arr.length;i++){
             boolean dupli=false;
             for(int j=i+1;j<arr.length;j++){
@@ -16,10 +143,18 @@ public class Dup{
             }
         }
 
+        return true;
+
     }
     public static void main(String args[]){
-        Scanner obj=new Scanner (System.in);
+         Scanner obj=new Scanner (System.in);
+        System.out.println("Enter size :");
+        int size=obj.nextInt();
+        int [] arr=new int [size];
+        for(int i=0;i<size;i++){
+            arr[i]=obj.nextInt();
+        }
 
-        dup();
+    dup(arr);
     }
 }
